fed4cc39eb468bdea7effe128d256ebd6e6d211b
rsevial <rebekahsevial@gmail.com>
2023-07-19 22:52:55 +0800

    Pseudocode for GUI window

--- GUI.py
+++ GUI.py
@@ -1,6 +1,14 @@
 # Programmed by: Rebekah Joy E. Sevial
-# Contact Tracing GUI
+# Contact Tracing using GUI
 
 # Import the module/s for creating GUI of the prgram
-# Create class for GUI
-# Create constructor for the class
+from tkinter import *
+# Create class that will show the main window of Application
+# Define function to create the initial layout of the Contact Tracing Application
+    # Create the main window of the program
+    # Title the program(its position, font, size and color)
+    # Create a button to add 
+    # Create a button to search
+    # Create a button to exit
+# call the mainloop
+
